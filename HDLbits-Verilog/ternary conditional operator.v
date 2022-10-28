3a95ee322aa2618750aec1ef9b71da7b97b6cc65
syfinnju <109597388+syfinnju@users.noreply.github.com>
2022-10-28 17:26:12 +0800

    Update ternary conditional operator.v

--- HDLbits-Verilog/ternary conditional operator.v
+++ HDLbits-Verilog/ternary conditional operator.v
@@ -1,3 +1,4 @@
+//first solution：通过使用3个串行mux进行连续比较
 module top_module (
     input [7:0] a, b, c, d,
     output [7:0] min);//
@@ -7,6 +8,27 @@ module top_module (
     assign f = e<c?e:c;
     assign min =f<d?f:d;
 
-    // assign intermediate_result1 = compare? true: false;
+endmodule
+
+
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);//
+    assign min =(a<b)&(a<c)&(a<d)?a:(b<a)&(b<c)&(b<d)?b:(c<b)&(c<a)&(c<d)?c:d;
+
+   
+endmodule
+
+//second solution：通过two-way的电路结构进行比较操作，主要比较上下两个way的值来确定输出min
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);//
+
+    wire [7:0] intermediate_result1;
+    wire [7:0] intermediate_result2;
+
+    assign intermediate_result1 = a<b? a: b;
+    assign intermediate_result2 = c<d? c: d;
+    assign min = intermediate_result1<intermediate_result2? intermediate_result1: intermediate_result2;
 
 endmodule
